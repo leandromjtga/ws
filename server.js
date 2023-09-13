395c59d30a584adb4cf633c010d1037825b72703
leandromjtga <leandromjtga@gmail.com>
2023-09-13 18:50:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const server = new WebSocket.Server({ port: 8080 });
+const server = new WebSocket.Server({ port: 3000 });
 const clients = new Map();
 
 server.on('connection', (socket) => {
